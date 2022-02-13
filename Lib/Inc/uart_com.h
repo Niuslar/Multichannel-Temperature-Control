1c8f3fddeab35efeadd58622c965149f10c4c64a
Niuslar <niuslar@outlook.com>
2022-02-13 13:18:35 +0000

    Add option to create instance with or without UART_DE Pin

--- Lib/Inc/uart_com.h
+++ Lib/Inc/uart_com.h
@@ -20,9 +20,14 @@ class CUartCom
 {
 private:
     UART_HandleTypeDef m_huart;
+    GPIO_TypeDef *m_uart_de_port = NULL;
+    uint16_t m_uart_de_pin;
 
 public:
     CUartCom(UART_HandleTypeDef &huart);
+    CUartCom(UART_HandleTypeDef &huart,
+             GPIO_TypeDef *uart_de_port,
+             uint16_t uart_de_pin);
 
     // I'm using C style strings because the UART HAL uses (uint8_t*) as
     // parameter
