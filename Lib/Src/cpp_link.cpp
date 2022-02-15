07277d1fc76789d65a8c18db6c8ffc01aa972bfe
Niuslar <niuslar@outlook.com>
2022-02-15 21:19:53 +0000

    Add test for thermistor object

--- Lib/Src/cpp_link.cpp
+++ Lib/Src/cpp_link.cpp
@@ -10,6 +10,8 @@
 #include "adc.h"
 #include "adc_data.h"
 #include "log.h"
+#include "stdio.h"
+#include "thermistor.h"
 #include "uart_com.h"
 
 #ifdef __cplusplus
@@ -27,9 +29,13 @@ extern "C"
         CAdcData adc_1(&hadc);
         adc_1.init();
 
+        CThermistor thermistor_1(adc_1, 0);
+
 #ifdef DEBUG
         log_main.log(CLog::LOG_INFO, "Entered cpp_main function");
 #endif
+
+        char buffer[20];
 
         // Infinite Loop
         while (1)
@@ -42,15 +48,20 @@ extern "C"
             uint16_t adc_ch_2 = adc_1[1];
             uint16_t adc_ch_14 = adc_1[13];
 
+            float temp = thermistor_1.getTemperature();
+
+            sprintf(buffer, "Temp: %.2f", temp);
+
             std::string value1 = "ADC1 = " + std::to_string(adc_ch_1);
             std::string value2 = "ADC2 = " + std::to_string(adc_ch_2);
             std::string value3 = "ADC14 = " + std::to_string(adc_ch_14);
 
+            log_main.log(CLog::LOG_INFO, buffer);
             log_main.log(CLog::LOG_INFO, value1);
             log_main.log(CLog::LOG_INFO, value2);
             log_main.log(CLog::LOG_INFO, value3);
 
-            HAL_Delay(2000);
+            HAL_Delay(1000);
             adc_1.trigger();
 #endif
         }
