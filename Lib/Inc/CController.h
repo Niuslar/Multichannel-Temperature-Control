246120a8677200acd4d39cf8960538ad82fa8bab
Salavat Magazov <s.magazov@erbamannheim.com>
2022-02-16 20:51:13 +0000

    feature: populate CController methods.

--- Lib/Inc/CController.h
+++ Lib/Inc/CController.h
@@ -13,7 +13,12 @@
 #ifndef CCONTROLLER_H_
 #define CCONTROLLER_H_
 
+#include <stdio.h>
+#include <string.h>
+
 #define MAX_NAME_SIZE 20
+// comment out if you don't want to collect runtime statistics
+#define COLLECT_STATS
 
 class CController
 {
@@ -21,16 +26,23 @@ public:
     CController(char *p_name, uint32_t run_period_ms);
     virtual ~CController();
 
-    virtual void tick(uint32_t current_time);
-    virtual bool run() = 0;
+    virtual bool tick(uint32_t current_time);
+    virtual void run();
     virtual bool newCommand(char *p_command);
+#ifdef COLLECT_STATS
+    virtual uint32_t getRunCalls() const;
+    virtual void resetRunCalls();
+#endif
 
 protected:
     char m_name[MAX_NAME_SIZE];
     uint32_t m_run_period_ms;
 
 private:
-    uint32_t m_previous_tick;
+    uint32_t m_previous_time;
+#ifdef COLLECT_STATS
+    uint32_t m_run_calls_counter;
+#endif
 };
 
 #endif /* CCONTROLLER_H_ */
--- Lib/Src/CController.cpp
+++ Lib/Src/CController.cpp
@@ -12,9 +12,16 @@
 
 #include "CController.h"
 
+/**
+ * @brief Create controller base class.
+ *
+ * @param p_name Name of the controller
+ * @param run_period_ms How often should run() method be called.
+ */
 CController::CController(char *p_name, uint32_t run_period_ms)
     : m_run_period_ms(run_period_ms),
-      m_previous_tick(0)
+      m_previous_time(0),
+      m_run_calls_counter(0)
 {
     strncpy(m_name, p_name, MAX_NAME_SIZE);
     m_name[MAX_NAME_SIZE - 1] = 0;
@@ -25,17 +32,59 @@ CController::~CController()
     // TODO Auto-generated destructor stub
 }
 
+/**
+ * @brief This method determines if it's time for the main loop of the
+ * controller to run.
+ *
+ * @param current_time Current time in milliseconds.
+ * @return True if it's time to execute run() method.
+ */
 bool CController::tick(uint32_t current_time)
 {
     bool b_time_to_run = false;
-    if ((current_tick - m_previous_tick) >= m_run_period_ms)
+    if ((current_time - m_previous_time) >= m_run_period_ms)
     {
         b_time_to_run = true;
     }
     return b_time_to_run;
 }
 
+void CController::run()
+{
+#ifdef COLLECT_STATS
+    m_run_calls_counter++;
+#endif
+    return;
+}
+
+/**
+ * @brief Give a new command to controller.
+ *
+ * @param p_command Pointer to NULL terminated command string.
+ * @return True if command was recognised.
+ */
 bool CController::newCommand(char *p_command)
 {
     return false;
 }
+
+#ifdef COLLECT_STATS
+/**
+ * @brief Get number of calls to run() method.
+ *
+ * @return Number of calls to run() methos since last reset.
+ */
+uint32_t CController::getRunCalls() const
+{
+    return m_run_calls_counter;
+}
+
+/**
+ * @brief Reset number of calls to run() method.
+ *
+ */
+void CController::resetRunCalls()
+{
+    m_run_calls_counter = 0;
+}
+#endif
