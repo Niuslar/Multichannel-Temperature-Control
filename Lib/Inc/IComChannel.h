b9cea7f0dc19c8233c2251255a2320ee6a2e1bb0
Salavat Magazov <s.magazov@erbamannheim.com>
2022-02-16 20:28:33 +0000

    feature: Create ComChannel interface class.

---
+++ Lib/Inc/IComChannel.h
@@ -0,0 +1,19 @@
+/*
+ * IComChannel.h
+ *
+ *  Created on: 16 Feb 2022
+ *      Author: salavat.magazov
+ */
+
+#ifndef INC_ICOMCHANNEL_H_
+#define INC_ICOMCHANNEL_H_
+
+class IComChannel
+{
+    virtual IComChannel() = 0;
+    virtual ~IComChannel() = 0;
+
+    // TODO: methods of this class are to be decided.
+};
+
+#endif /* INC_ICOMCHANNEL_H_ */
