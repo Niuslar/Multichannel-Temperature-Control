79070f272203b1ce7cb917f0c2e6833d5d7f16ef
Salavat Magazov <s.magazov@erbamannheim.com>
2022-02-19 18:52:43 +0000

    docs: add doxygen headers housekeeping: minor cleanup

--- Lib/Inc/CExpFilter.h
+++ Lib/Inc/CExpFilter.h
@@ -22,25 +22,56 @@ public:
 
     void setOrder(float filter_order);
     void reset();
+    /**
+     * @brief Get last instant value.
+     *
+     * @return Last instant value.
+     */
     float getLast() const
     {
         return m_last;
     };
+
+    /**
+     * @brief Get current running average.
+     *
+     * @return Current running average.
+     */
     float getAverage() const
     {
         return m_average;
     };
+
     float getAverage(float instant_value);
 
+    /**
+     * @brief Get instant rate of change of filtered value.
+     * @note This filter is not very good at providing good average rate of
+     * change estimate. If you want good rate of change, then window average
+     * filter is much better.
+     *
+     * @return Difference between current and previous average value.
+     */
     float getRate() const
     {
         return m_rate;
     };
 #ifdef TRACK_EXTREMA
+    /**
+     * @brief Get running minimum of the instant value.
+     *
+     * @return Current value of the running minimum.
+     */
     float getMin() const
     {
         return m_min;
     };
+
+    /**
+     * @brief Get running maximum of the instant value.
+     *
+     * @return Current value of the running maximum.
+     */
     float getMax() const
     {
         return m_max;
--- Lib/Src/CExpFilter.cpp
+++ Lib/Src/CExpFilter.cpp
@@ -10,14 +10,23 @@
  */
 
 #include "CExpFilter.h"
+#include <math.h>
 #include <stdlib.h>
 #include <limits>
-#include <math.h>
 
 /**
  * @brief Create a filter with specified order.
  *
- * @param filter_order
+ * @param filter_order Power of the filter.
+ * @note The filter coefficient is calculated as
+ * 1 - 10^(-filter_order). Example
+ * filter_order = 1, filter coefficient = 0.9
+ * filter_order = 2, filter coefficient = 0.99
+ * filter_order = 5, filter coefficient = 0.99999
+ * Essentially the higher the filter order the "slower" the filter becomes. The
+ * response of the filter also depends on frequency with which new data is being
+ * fed to it. Ideally, you want reasonably high frequency of fresh samples, but
+ * that brings higher overhead of calculating new average values.
  */
 CExpFilter::CExpFilter(float filter_order)
 {
@@ -25,12 +34,24 @@ CExpFilter::CExpFilter(float filter_order)
     reset();
 }
 
+/**
+ * @brief Set the order of the filter after construction.
+ * @note The change will not affect past samples, but will take immediate effect
+ * from next sample forward.
+ *
+ * @param filter_order New filter order. See ctor notes for detailed
+ * explanation.
+ */
 void CExpFilter::setOrder(float filter_order)
 {
     filter_order = abs(filter_order);
     m_coefficient = 1 - pow(10, -filter_order);
 }
 
+/**
+ * @brief Reset the filter to freshly constructed state.
+ * @note The reset is not going to affect filter order.
+ */
 void CExpFilter::reset()
 {
     m_last = 0;
@@ -45,10 +66,16 @@ void CExpFilter::reset()
 #endif
 }
 
+/**
+ * @brief Calculate new average and return it.
+ *
+ * @param instant_value New instant value.
+ * @return Newly calculated running average.
+ */
 float CExpFilter::getAverage(float instant_value)
 {
     /* if all three values are zero, chances are this is the first sample, so
-     * skip calculations. */
+     * skip calculations and initialise filter with first sample. */
     if ((m_last == 0) && (m_average == 0) && (m_rate == 0))
     {
         m_average = instant_value;
