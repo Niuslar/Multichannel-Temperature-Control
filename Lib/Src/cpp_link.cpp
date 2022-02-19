bb8baca16aa7e61e8b69277bc338a10c47394cfb
Niuslar <niuslar@outlook.com>
2022-02-19 20:29:46 +0000

    Get cpp_link.cpp from develop

--- Lib/Src/cpp_link.cpp
+++ Lib/Src/cpp_link.cpp
@@ -8,10 +8,21 @@
  */
 
 #include "CAdcData.h"
+#include "CDispatcher.h"
 #include "CLog.h"
-#include "CThermistor.h"
 #include "CUartCom.h"
 #include "adc.h"
+
+/** @note: Instantiating classes here means the ctor for each class will run
+ * before any of the setup code is being executed. Therefore, ctor that relies
+ * on configured hardware will fail. If class that is meant to run hardware is
+ * instantiated, then ctor must only do bare essentials. Once cpp_main() starts
+ * executing relevant classes can call initialisation methods that will
+ * configure the hardware.
+ */
+CUartCom g_debug_uart(&huart2);
+CLog g_logger(&g_debug_uart, "Debug");
+CDispatcher g_dispatcher(&g_logger);
 
 #ifdef __cplusplus
 extern "C"
@@ -20,55 +31,54 @@ extern "C"
 
     void cpp_main()
     {
-        CUartCom st_link_uart(&huart2);
+        /**
+         * @note By this point in the code all hardware has been configured. Run
+         * initialisation code here.
+         */
+        //        CUartCom uart_for_errors(&huart2);
 
-        CLog log_main(&st_link_uart, "Main");
+        //        CLog log_main(&uart_for_errors, "Main");
+        //        log_main.setLogLevel(CLog::LOG_INFO);
 
-        log_main.setLogLevel(CLog::LOG_INFO);
-
-        CAdcData adc_1(&hadc);
-        adc_1.init();
-
-        CLog log_thermistor(&st_link_uart, "Thermistor");
-
-        CThermistor thermistor_1;
+        //        CAdcData adc_1(&hadc);
+        //        adc_1.init();
 
 #ifdef DEBUG
-        log_main.log(CLog::LOG_INFO, "Entered cpp_main function");
+        g_logger.log(CLog::LOG_INFO, "Entered cpp_main function");
 #endif
-
-        char buffer[20];
 
         // Infinite Loop
-        while (1)
-        {
-#ifdef DEBUG
-            // Test 3 of the 14 ADC channels
 
-            // First read channels
-            float adc_ch_1 = adc_1[0];
-            uint16_t adc_ch_2 = adc_1[1];
-            uint16_t adc_ch_14 = adc_1[13];
+        /**
+         * @note Dispatcher run() method will not return. At this point
+         * scheduling of controllers will start.
+         */
+        g_dispatcher.run();
+        //        while (1)
+        //        {
+        //#ifdef DEBUG
+        //            // Test 3 of the 14 ADC channels
+        //
+        //            // First read channels
+        //            uint16_t adc_ch_1 = adc_1[0];
+        //            uint16_t adc_ch_2 = adc_1[1];
+        //            uint16_t adc_ch_14 = adc_1[13];
+        //
+        //            std::string value1 = "ADC1 = " + std::to_string(adc_ch_1);
+        //            std::string value2 = "ADC2 = " + std::to_string(adc_ch_2);
+        //            std::string value3 = "ADC14 = " +
+        //            std::to_string(adc_ch_14);
+        //
+        //            log_main.log(CLog::LOG_INFO, value1);
+        //            log_main.log(CLog::LOG_INFO, value2);
+        //            log_main.log(CLog::LOG_INFO, value3);
+        //
+        //            HAL_Delay(2000);
+        //            adc_1.trigger();
+        //#endif
+        //        }
 
-            float temp = thermistor_1.getTemperature(adc_ch_1);
-
-            sprintf(buffer, "Temp: %.2f", temp);
-
-            std::string value1 = "ADC1 = " + std::to_string(adc_ch_1);
-            std::string value2 = "ADC2 = " + std::to_string(adc_ch_2);
-            std::string value3 = "ADC14 = " + std::to_string(adc_ch_14);
-
-            log_main.log(CLog::LOG_INFO, buffer);
-            log_main.log(CLog::LOG_INFO, value1);
-            log_main.log(CLog::LOG_INFO, value2);
-            log_main.log(CLog::LOG_INFO, value3);
-
-            HAL_Delay(1000);
-            adc_1.trigger();
-#endif
-        }
-
-        log_main.log(CLog::LOG_ERROR, "Reached end of cpp_main()");
+        g_logger.log(CLog::LOG_ERROR, "Reached end of cpp_main()");
     }
 
 #ifdef __cplusplus
