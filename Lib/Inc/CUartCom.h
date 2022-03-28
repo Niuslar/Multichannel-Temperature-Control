73764f1058a414ec0f270ddff9f00d97c5f2b6d9
Niuslar <niuslar@outlook.com>
2022-03-28 13:36:57 +0100

    Add methods to CUartCom to receive data

--- Lib/Inc/CUartCom.h
+++ Lib/Inc/CUartCom.h
@@ -11,13 +11,15 @@
 #ifndef CUARTCOM_H_
 #define CUARTCOM_H_
 
+#include <queue>
 #include <string>
+#include "CFifoBuffer.h"
 #include "CGpioWrapper.h"
 #include "IComChannel.h"
 #include "usart.h"
 
 #define UART_TIMEOUT 100
-
+#define QUEUE_SIZE   20
 class CUartCom : public IComChannel
 {
 public:
@@ -27,13 +29,21 @@ public:
              GPIO_TypeDef *uart_de_port,
              uint16_t uart_de_pin);
 
-    void send(std::string msg);
+    void startReception();
+    void send(const std::string &msg);
     bool isCommandAvailable();
     std::string getCommand();
+    void uartRxHandler(UART_HandleTypeDef *p_huart);
+
+    static CUartCom *sp_UART_1;
+    static CUartCom *sp_UART_2;
 
 private:
     UART_HandleTypeDef *mp_huart;
     CGpioWrapper m_uart_de_pin;
+    CFifoBuffer m_rx_buffer;
+    uint8_t m_rx_char;
+    std::queue<std::string> m_cmd_queue;
 };
 
 #endif /* CUARTCOM_H_ */
