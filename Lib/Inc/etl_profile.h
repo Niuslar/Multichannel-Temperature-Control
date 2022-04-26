54ccb3c2e71ecea7ea5a87cf3e140dff8c8d6f72
Salavat Magazov <s.magazov@erbamannheim.com>
2022-04-26 15:13:51 +0100

    fix: typo in filename.

---
+++ Lib/Inc/etl_profile.h
@@ -0,0 +1,13 @@
+/**
+ * @file etl_profile.h
+ *
+ *  Created on: 12 Apr 2022
+ *      Author: niuslar
+ */
+
+#ifndef ETL_PROFILE_H_
+#define ETL_PROFILE_H_
+
+#define ETL_COMPILER_ARM
+
+#endif /* ETL_PROFILE_H_ */
