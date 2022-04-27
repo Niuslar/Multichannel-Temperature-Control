06d79076a38810393cebf4b40ee6f9aac1c306b3
Salavat Magazov <s.magazov@erbamannheim.com>
2022-04-27 12:31:16 +0100

    wip: populate temperature commands

--- Controllers/CTemperatureController.cpp
+++ Controllers/CTemperatureController.cpp
@@ -19,11 +19,29 @@ CTemperatureController::CTemperatureController(
 {
 }
 
+/**
+ * @brief Run all repeated temperature control activities.
+ *
+ */
 void CTemperatureController::run() {}
 
 bool CTemperatureController::newCommand(ICommand *p_command,
                                         IComChannel *p_comchannel)
 {
-    return false;
+    bool b_command_recognised = false;
+    etl::string<MAX_STRING_SIZE> command_name;
+    command_name = p_command->getName();
+    /* generic query about current state of all active channels
+     *?temperature()
+     */
+    if (command_name.compare("?temperature"))
+    {
+        // todo: compile temperature report and send via p_comchannel.
+    }
+    /* set target temperature [for specific channel]. If channel argument is
+     * missing, target is applied to all channels.
+     *  temperature(target, [channel])
+     */
+    if (command_name.compare("temperature")) return b_command_recognised;
 }
 void CTemperatureController::reset() {}
