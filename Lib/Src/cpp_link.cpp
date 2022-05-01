1c5a2edefa9dfe2af3b60784ba1f0ee8fd07b96c
Salavat Magazov <s.magazov@erbamannheim.com>
2022-05-01 02:29:32 +0100

    feature: add skeleton structure of the hardware abstraction layer.

--- Lib/Src/cpp_link.cpp
+++ Lib/Src/cpp_link.cpp
@@ -10,6 +10,8 @@
 #include "CAdcData.h"
 #include "CDebugController.h"
 #include "CDispatcher.h"
+#include "CMockHardwareMap.h"
+#include "CRealHardwareMap.h"
 #include "CUartCom.h"
 #include "adc.h"
 
@@ -27,6 +29,18 @@
 CUartCom g_debug_uart(&huart2);
 CDispatcher g_dispatcher(&g_debug_uart);
 
+/* hardware map */
+/**
+ * @note This compile time switch allows for defining of whether a real
+ * peripherals are used or mock ones. Mock peripherals allow for the logic to be
+ * ran without engaging peripherals. I
+ */
+#ifdef MOCK_HARDWARE
+CMockHardwareMap g_hardware_map;
+#else
+CRealHardwareMap g_hardware_map;
+#endif
+
 /* controllers */
 CDebugController g_debug_controller("debug", 100);
 
@@ -37,6 +51,7 @@ extern "C"
 
     void cpp_main()
     {
+        g_hardware_map.init();
         /**
          * @note By this point in the code all hardware has been configured. Run
          * initialisation code here.
