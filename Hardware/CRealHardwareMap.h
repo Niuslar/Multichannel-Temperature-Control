76de70cad563d7934994151bb3716ac733085cdc
Salavat Magazov <s.magazov@erbamannheim.com>
2022-05-01 19:30:08 +0100

    feature (CRealHardwareMap): add hard pwm hardware mapping and initialisation.

--- Hardware/CRealHardwareMap.h
+++ Hardware/CRealHardwareMap.h
@@ -16,6 +16,7 @@
 #include "IHardwareMap.h"
 
 #include "CAdcData.h"
+#include "CHardPwmOutput.h"
 
 class CRealHardwareMap : public IHardwareMap
 {
@@ -30,8 +31,17 @@ public:
     virtual float getAmbientTemp() const;
     virtual float getChanneTemp(uint8_t channel) const;
 
+    virtual float setPwmOutput(float power, uint8_t channel);
+
 private:
     CAdcData m_adc;
+    typedef struct TIMER_INIT_MAP_T
+    {
+        TIM_HandleTypeDef *p_timer;
+        uint8_t channel;
+    } timer_init_map_t;
+    static const timer_init_map_t s_timer_init_map[HARD_PWM_OUTPUTS];
+    CHardPwmOutput m_pwm_output[HARD_PWM_OUTPUTS];
 };
 
 #endif /* CREALHARDWAREMAP_H_ */
