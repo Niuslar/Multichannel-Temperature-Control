b5609620ee44c4e1e04f792c7e601b5b1dc6731d
Salavat Magazov <s.magazov@erbamannheim.com>
2022-05-02 13:22:27 +0100

    feature (CMockHardwareMap): add skeleton methods to mock hardware class.

--- Hardware/CMockHardwareMap.cpp
+++ Hardware/CMockHardwareMap.cpp
@@ -21,3 +21,57 @@ void CMockHardwareMap::init()
 {
     /* all mock initialisation of mock hardware layer goes here. */
 }
+
+float getInputVoltage() const
+{
+    return 24.0;
+}
+
+float getTotalCurrent() const
+{
+    // TODO: model current consumption based control current + some for digital
+    // logic.
+    return getControlCurrent() + 0.1;
+}
+
+float getControlCurrent() const
+{
+    // TODO: model current consumption based on number of heaters powered on.
+}
+
+float getAmbientTemp() const
+{
+    // TODO: consider modelling this.
+    return 21.0;
+}
+
+float getChanneTemp(uint8_t channel) const
+{
+    return getAmbientTemp();
+}
+
+float setHardPwmOutput(float power, uint8_t channel)
+{
+    // TODO: This sets power parameters for the model.
+    return 0;
+}
+
+float getHardPwmOutput(uint8_t channel)
+{
+    // TODO: this just returns what's been recorded for heater power
+    return 0;
+}
+
+#ifdef SOFT_PWM_OUTPUTS
+float setSoftPwmOutput(float power, uint8_t channel) = 0;
+float getSoftPwmOutput(uint8_t channel) = 0;
+#endif
+
+void setBreathingLight(float duty_cycle);
+
+void enableControlPower(bool b_enable)
+{
+    // todo: this should probably participate in the model of control current
+    // flow and temperature control.
+}
+
--- Hardware/CMockHardwareMap.h
+++ Hardware/CMockHardwareMap.h
@@ -21,6 +21,25 @@ public:
     CMockHardwareMap();
 
     virtual void init();
+
+    virtual float getInputVoltage() const;
+    virtual float getTotalCurrent() const;
+    virtual float getControlCurrent() const;
+    virtual float getAmbientTemp() const;
+    virtual float getChanneTemp(uint8_t channel) const;
+
+    virtual float setHardPwmOutput(float power, uint8_t channel);
+    virtual float getHardPwmOutput(uint8_t channel);
+
+#ifdef SOFT_PWM_OUTPUTS
+    virtual float setSoftPwmOutput(float power, uint8_t channel) = 0;
+    virtual float getSoftPwmOutput(uint8_t channel) = 0;
+#endif
+
+    virtual void setBreathingLight(float duty_cycle);
+
+    virtual void enableControlPower(bool b_enable);
+
 };
 
 #endif /* CMOCKHARDWAREMAP_H_ */
