9979a6916ba2aeff2ab34b9a47eab22433b0939a
Niuslar <niuslar@outlook.com>
2022-05-11 10:50:42 +0100

    wip

---
+++ Lib/Src/CJsonParser.cpp
@@ -0,0 +1,235 @@
+/**
+ * @file CJsonParser.cpp
+ */
+/*
+ * Created on : Apr 4, 2022
+ * Author : niuslar
+ */
+
+#include "CJsonParser.h"
+
+#define ERROR_VALUE (0.00)
+
+/**
+ * @brief Constructor
+ */
+CJsonParser::CJsonParser() {}
+
+/**
+ * @brief Parse string
+ * @param String to be parsed
+ * @return Last parsing state
+ */
+void CJsonParser::parse(const etl::string<MAX_STRING_SIZE> &string)
+{
+    // Reset command variables
+    m_argument_counter = 0;
+    m_command_name = "";
+
+    // Get tokens from string
+    getTokens(string);
+
+    for (uint16_t i = 0; i < m_token_counter; i++)
+    {
+        token_t current_token = m_tokens[i];
+    }
+}
+
+/**
+ * @brief Scan a string to get tokens*
+ * @param String
+ * @Note a lexer, tokeniser or scanner takes a string and breaks it up into
+ * tokens. A token is the basic building block of the language. For example, in
+ * English single words and punctuation would be tokens.
+ */
+void CJsonParser::getTokens(const etl::string<MAX_STRING_SIZE> &string)
+{
+    // Reset variables
+    m_tokens.clear();
+    m_token_counter = 0;
+    token_t current_token;
+
+    // Loop through characters in string
+    for (char current_char : string)
+    {
+        switch (current_char)
+        {
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                if (current_token.type == STRING ||
+                    current_token.type == INTEGER ||
+                    current_token.type == FLOAT)
+                {
+                    current_token.text.append(1, current_char);
+                }
+                else
+                {
+                    endToken(current_token);
+                    current_token.text.append(1, current_char);
+                    current_token.type = INTEGER;
+                }
+                break;
+            case '.':
+                if (current_token.type == INTEGER)
+                {
+                    current_token.text.append(1, current_char);
+                    current_token.type = FLOAT;
+                }
+                else
+                {
+                    endToken(current_token);
+                    current_token.text.append(1, current_char);
+                    current_token.type = INVALID;
+                }
+                break;
+            case ' ':
+            case '\t':
+                if (current_token.type == STRING)
+                {
+                    current_token.text.append(1, current_char);
+                }
+                else
+                {
+                    endToken(current_token);
+                }
+                break;
+            case '\n':
+            case '\0':
+            case '\r':
+                endToken(current_token);
+                break;
+            case ':':
+                addSpecialChar(current_token, COLON, current_char);
+                break;
+            case '{':
+                addSpecialChar(current_token, CURLY_OPEN, current_char);
+                break;
+            case '}':
+                addSpecialChar(current_token, CURLY_CLOSE, current_char);
+                break;
+            case '[':
+                addSpecialChar(current_token, ARRAY_OPEN, current_char);
+                break;
+            case ']':
+                addSpecialChar(current_token, ARRAY_CLOSE, current_char);
+                break;
+            case ',':
+                addSpecialChar(current_token, COMMA, current_char);
+                break;
+
+            case '"':
+                if (current_token.type != STRING)
+                {
+                    endToken(current_token);
+                    current_token.type = STRING;
+                }
+                else
+                {
+                    endToken(current_token);
+                }
+                break;
+
+            default:
+                if (current_token.type == WHITESPACE ||
+                    current_token.type == INTEGER ||
+                    current_token.type == FLOAT)
+                {
+                    endToken(current_token);
+                    current_token.text.append(1, current_char);
+                    current_token.type = INVALID;
+                }
+                else
+                {
+                    current_token.text.append(1, current_char);
+                }
+                break;
+        }
+    }
+    // Finish any token that might have not been stored
+    endToken(current_token);
+}
+
+/**
+ * @brief Add token to m_tokens vector
+ * @param token to be added
+ */
+void CJsonParser::endToken(CJsonParser::token_t &token)
+{
+    if (token.type != WHITESPACE)
+    {
+        if (token.text.empty() == false)
+        {
+            m_tokens.push_back(token);
+            m_token_counter++;
+        }
+    }
+    token.type = WHITESPACE;
+    token.text.clear();
+}
+
+/**
+ * @brief Get name of the last parsed command.
+ *
+ * @return Name of the last parsed command.
+ */
+etl::string<MAX_STRING_SIZE> CJsonParser::getName() const
+{
+    return m_command_name;
+}
+
+/**
+ * @brief Get number of arguments.
+ *
+ * @return Number of parsed arguments.
+ */
+unsigned int CJsonParser::getArgumentCount() const
+{
+    return m_argument_counter;
+}
+
+/**
+ * @brief Access parsed arguments by index of their position in the command
+ * string.
+ *
+ * @param index Position of the argument to be accessed. Zero-based index.
+ * @return Value of the argument at the requested index.
+ */
+float CJsonParser::operator[](unsigned int index)
+{
+    if (index < MAX_ARGUMENT_COUNT)
+    {
+        return m_arguments[index];
+    }
+    return ERROR_VALUE;
+}
+
+/**
+ * @brief Add special character token
+ * @param token
+ * @param token type
+ * @param character to be appended
+ */
+void CJsonParser::addSpecialChar(CJsonParser::token_t &token,
+                                 CJsonParser::token_type_t operator_type,
+                                 char character)
+{
+    if (token.type != STRING)
+    {
+        endToken(token);
+        token.type = operator_type;
+        token.text.append(1, character);
+        endToken(token);
+    }
+    else
+    {
+        token.text.append(1, character);
+    }
+}
--- Lib/Src/cpp_link.cpp
+++ Lib/Src/cpp_link.cpp
@@ -11,7 +11,7 @@
 #include "CAdcData.h"
 #include "CDebugController.h"
 #include "CDispatcher.h"
-#include "CParser.h"
+#include "CJsonParser.h"
 #include "CUartCom.h"
 #include "adc.h"
 
@@ -19,29 +19,28 @@
 #    warning The software PWM has not been tested, do not use without testing.
 #endif
 
-etl::string<60> parsing_state[] = {"IDLE",
-                                   "BEGIN_KEY",
-                                   "END_KEY",
-                                   "BEGIN_VALUE",
-                                   "BEGIN_ARRAY",
-                                   "ADD_VALUE",
-                                   "VALUE_ADDED",
-                                   "END_VALUES",
-                                   "COMMAND_OK",
-                                   "ERROR_MAX_ARGS",
-                                   "ERROR_NESTED_JSON",
-                                   "ERROR"};
+etl::string<60> token_types[] = {"WHITESPACE",
+                                 "INTEGER",
+                                 "FLOAT",
+                                 "CURLY_OPEN",
+                                 "CURLY_CLOSE",
+                                 "COLON",
+                                 "STRING",
+                                 "ARRAY_OPEN",
+                                 "ARRAY_CLOSE",
+                                 "COMMA",
+                                 "INVALID"};
 
 /** @note: Instantiating classes here means the ctor for each class will run
- * before any of the setup code is being executed. Therefore, ctor that relies
- * on configured hardware will fail. If class that is meant to run hardware is
- * instantiated, then ctor must only do bare essentials. Once cpp_main() starts
- * executing relevant classes can call initialisation methods that will
- * configure the hardware.
+ * before any of the setup code is being executed. Therefore, ctor that
+ * relies on configured hardware will fail. If class that is meant to run
+ * hardware is instantiated, then ctor must only do bare essentials. Once
+ * cpp_main() starts executing relevant classes can call initialisation
+ * methods that will configure the hardware.
  */
 CUartCom g_debug_uart("Main");
 CDispatcher g_dispatcher(&g_debug_uart);
-CParser g_parser;
+CJsonParser g_parser;
 
 /* controllers */
 CDebugController g_debug_controller("debug", 100);
@@ -71,33 +70,14 @@ extern "C"
         {
             if (g_debug_uart.isDataAvailable())
             {
-                CParser::parser_state_t state =
-                    g_parser.parse(g_debug_uart.getData());
-                if (state == CParser::COMMAND_OK)
+                g_parser.getTokens(g_debug_uart.getData());
+                for (CJsonParser::token_t token : g_parser.m_tokens)
                 {
-                    g_debug_uart.send("Command name: ");
-                    g_debug_uart.send(g_parser.getName());
-                    g_debug_uart.send("\n");
-                    unsigned int arg_count = g_parser.getArgumentCount();
-                    for (unsigned int i = 0; i < arg_count; i++)
-                    {
-                        g_debug_uart.send("Argument ");
-                        etl::string<10> index;
-                        etl::to_string((i + 1), index);
-                        g_debug_uart.send(index);
-                        g_debug_uart.send(": ");
-                        etl::string<30> argument;
-                        etl::to_string(g_parser[i],
-                                       argument,
-                                       etl::format_spec().precision(1),
-                                       true);
-                        g_debug_uart.send(argument);
-                        g_debug_uart.send("\n");
-                    }
-                }
-                else
-                {
-                    g_debug_uart.send(parsing_state[state]);
+                    g_debug_uart.send("Token: ");
+                    g_debug_uart.send(token.text);
+                    g_debug_uart.send(" | ");
+                    g_debug_uart.send("Type: ");
+                    g_debug_uart.send(token_types[token.type]);
                     g_debug_uart.send("\n");
                 }
             }
