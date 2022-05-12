5fc19d87d97ad169c959e49e39dd2121c399ad76
Niuslar <niuslar@outlook.com>
2022-05-12 14:59:52 +0100

    feature: restore cpp_link.cpp from develop

--- Lib/Src/cpp_link.cpp
+++ Lib/Src/cpp_link.cpp
@@ -7,11 +7,11 @@
  *      Author: niuslar
  */
 
-#include "../etl/to_string.h"
 #include "CAdcData.h"
 #include "CDebugController.h"
 #include "CDispatcher.h"
-#include "CJsonParser.h"
+#include "CMockHardwareMap.h"
+#include "CRealHardwareMap.h"
 #include "CUartCom.h"
 #include "adc.h"
 
@@ -19,28 +19,27 @@
 #    warning The software PWM has not been tested, do not use without testing.
 #endif
 
-etl::string<60> token_types[] = {"WHITESPACE",
-                                 "INTEGER",
-                                 "FLOAT",
-                                 "CURLY_OPEN",
-                                 "CURLY_CLOSE",
-                                 "COLON",
-                                 "STRING",
-                                 "ARRAY_OPEN",
-                                 "ARRAY_CLOSE",
-                                 "COMMA",
-                                 "INVALID"};
-
 /** @note: Instantiating classes here means the ctor for each class will run
- * before any of the setup code is being executed. Therefore, ctor that
- * relies on configured hardware will fail. If class that is meant to run
- * hardware is instantiated, then ctor must only do bare essentials. Once
- * cpp_main() starts executing relevant classes can call initialisation
- * methods that will configure the hardware.
+ * before any of the setup code is being executed. Therefore, ctor that relies
+ * on configured hardware will fail. If class that is meant to run hardware is
+ * instantiated, then ctor must only do bare essentials. Once cpp_main() starts
+ * executing relevant classes can call initialisation methods that will
+ * configure the hardware.
  */
 CUartCom g_debug_uart("Main");
 CDispatcher g_dispatcher(&g_debug_uart);
-CJsonParser g_parser;
+
+/* hardware map */
+/**
+ * @note This compile time switch allows for defining of whether a real
+ * peripherals are used or mock ones. Mock peripherals allow for the logic to be
+ * ran without engaging peripherals. I
+ */
+#ifdef MOCK_HARDWARE
+CMockHardwareMap g_hardware_map;
+#else
+CRealHardwareMap g_hardware_map;
+#endif
 
 /* controllers */
 CDebugController g_debug_controller("debug", 100);
@@ -52,6 +51,7 @@ extern "C"
 
     void cpp_main()
     {
+        g_hardware_map.init();
         /**
          * @note By this point in the code all hardware has been configured. Run
          * initialisation code here.
@@ -62,42 +62,15 @@ extern "C"
         //        CLog log_main(&uart_for_errors, "Main");
         //        log_main.setLogLevel(CLog::LOG_INFO);
 
-        g_debug_uart.init(&huart2);
-        g_debug_uart.startRx();
+        //        CAdcData adc_1(&hadc);
+        //        adc_1.init();
 
         // Infinite Loop
-        while (1)
-        {
-            if (g_debug_uart.isDataAvailable())
-            {
-                if (g_parser.parse(g_debug_uart.getData()))
-                {
-                    g_debug_uart.send("Command name: ");
-                    g_debug_uart.send(*g_parser.getName());
-                    g_debug_uart.send("\n");
-                    unsigned int arg_count = g_parser.getArgumentCount();
-                    for (unsigned int i = 0; i < arg_count; i++)
-                    {
-                        g_debug_uart.send("Argument ");
-                        etl::string<10> index;
-                        etl::to_string((i + 1), index);
-                        g_debug_uart.send(index);
-                        g_debug_uart.send(": ");
-                        etl::string<30> argument;
-                        etl::to_string(g_parser[i],
-                                       argument,
-                                       etl::format_spec().precision(1),
-                                       true);
-                        g_debug_uart.send(argument);
-                        g_debug_uart.send("\n");
-                    }
-                }
-                else
-                {
-                    g_debug_uart.send("Could not process command\n");
-                }
-            }
-        }
+
+        /**
+         * @note Dispatcher run() method will not return. At this point
+         * scheduling of controllers will start.
+         */
         g_dispatcher.run();
         //        while (1)
         //        {
