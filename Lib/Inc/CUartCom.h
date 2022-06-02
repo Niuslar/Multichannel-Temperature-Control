e459df5332602fd2726cf9e854dafbf10dd57580
Niuslar <niuslar@outlook.com>
2022-06-02 23:09:39 +0100

    refactor: use etl::strings with static memory allocation

--- Lib/Inc/CUartCom.h
+++ Lib/Inc/CUartCom.h
@@ -12,25 +12,68 @@
 #define CUARTCOM_H_
 
 #include "../etl/string.h"
+#include "CFIFOBuffer.h"
+#include "CGpioWrapper.h"
+#include "IComChannel.h"
 #include "usart.h"
 
-#define MAX_STRING_SIZE 60
-#define UART_TIMEOUT    100
+#define UART_TIMEOUT      100
+#define MAX_RX_QUEUE_SIZE 40
+#define MAX_TX_QUEUE_SIZE 40
+#define MAX_UART_ENGINES  8
+#define TX_BUF_SIZE       60
+#define RX_BUF_SIZE       60
 
-class CUartCom
+class CUartCom : public IComChannel
 {
-private:
+public:
+    CUartCom(const etl::string<MAX_STRING_SIZE> name);
+
+    bool init(UART_HandleTypeDef *p_huart);
+    bool init(UART_HandleTypeDef *p_huart,
+              GPIO_TypeDef *uart_de_port,
+              uint16_t uart_de_pin);
+
+    void startRx();
+    void send(const etl::string<MAX_STRING_SIZE> msg);
+    bool isCommandAvailable();
+    etl::string<MAX_STRING_SIZE> getCommand();
+    void uartRxHandler(UART_HandleTypeDef *p_huart);
+    void uartTxHandler(UART_HandleTypeDef *p_huart);
+
+    static int8_t getIndex(UART_HandleTypeDef *p_huart);
+
+    static CUartCom *sp_UART[MAX_UART_ENGINES];
+    static uint8_t s_uart_instances;
     UART_HandleTypeDef *mp_huart;
     GPIO_TypeDef *m_uart_de_port = nullptr;
     uint16_t m_uart_de_pin;
 
-public:
-    CUartCom(UART_HandleTypeDef *p_huart);
-    CUartCom(UART_HandleTypeDef *p_huart,
-             GPIO_TypeDef *uart_de_port,
-             uint16_t uart_de_pin);
+private:
+    void updateTxBuffer();
+    void endTx();
+    void transmit();
+    etl::string<MAX_STRING_SIZE> getString();
+    enum uart_status
+    {
+        IDLE,
+        TX
+    };
+    enum uart_events
+    {
+        NO_MESSAGE_AVAILABLE,
+        MESSAGE_AVAILABLE
+    };
 
-    void send(const etl::string<MAX_STRING_SIZE> &msg);
+    bool mb_half_duplex = false;
+    uint8_t m_status = IDLE;
+    CGpioWrapper m_uart_de_pin;
+    CFIFOBuffer<char, RX_BUF_SIZE> m_rx_buffer;
+    char m_tx_buffer[TX_BUF_SIZE] = {0};
+    uint8_t m_tx_msg_length = 0;
+    uint8_t m_rx_char;
+    CFIFOBuffer<etl::string<MAX_STRING_SIZE>, MAX_RX_QUEUE_SIZE> m_rx_queue;
+    CFIFOBuffer<etl::string<MAX_STRING_SIZE>, MAX_TX_QUEUE_SIZE> m_tx_queue;
 };
 
 #endif /* CUARTCOM_H_ */
--- Lib/Src/CUartCom.cpp
+++ Lib/Src/CUartCom.cpp
@@ -14,22 +14,72 @@ CUartCom *CUartCom::sp_UART[MAX_UART_ENGINES] = {nullptr};
 uint8_t CUartCom::s_uart_instances = 0;
 
 /**
- * @brief Construct UART communication object.
- *
+ * @brief Construct UART communication instance
+ * @param name Name of the instance
+ * @note An instance needs to be initialised using the init() method to
+ * send and receive data
+ */
+CUartCom::CUartCom(const etl::string<MAX_STRING_SIZE> name)
+    : IComChannel(name),
+      mp_huart(nullptr),
+      m_uart_de_pin()
+{
+}
+
+/**
+ * @brief Initalise UART communication object
  * @param p_huart Pointer to UART hardware control register structure.
  */
-CUartCom::CUartCom(UART_HandleTypeDef *p_huart) : mp_huart(p_huart)
+bool CUartCom::init(UART_HandleTypeDef *p_huart)
 {
-    // Check mp_huart is not null
-    if (!mp_huart)
+    return init(p_huart, nullptr, 100);
+}
+
+/**
+ * @brief Initalise UART communication object with half-duplex mode.
+ * @param p_huart Pointer to UART hardware control register structure.
+ * @param uart_de_port Pointer to GPIO port.
+ * @param uart_de_pin GPIO pin.
+ */
+bool CUartCom::init(UART_HandleTypeDef *p_huart,
+                    GPIO_TypeDef *uart_de_port,
+                    uint16_t uart_de_pin)
+{
+    bool b_success = true;
+
+    if (!p_huart)
     {
-        // Hang application with a toggling ALARM Pin
-        while (1)
+        Error_Handler();
+        b_success = false;
+    }
+    else
+    {
+        mp_huart = p_huart;
+        if (uart_de_port != nullptr)
         {
-            // TODO: cannot use alarm pin. No space for it due to one less pin
-            // available on this MCU. HAL_GPIO_TogglePin(ALARM_GPIO_Port,
-            // ALARM_Pin);
-            HAL_Delay(200);
+            // When UART_DE pin is used, half-duplex is assumed
+            mb_half_duplex = true;
+        }
+        m_uart_de_pin.init(uart_de_port, uart_de_pin);
+
+        // Check this hardware is not assigned to another instance
+        int8_t index = CUartCom::getIndex(p_huart);
+        if (index >= 0)
+        {
+            b_success = false;
+        }
+        else
+        {
+            // Assign hardware to instance
+            if (s_uart_instances < MAX_UART_ENGINES)
+            {
+                sp_UART[s_uart_instances] = this;
+                s_uart_instances++;
+            }
+            else
+            {
+                b_success = false;
+            }
         }
     }
 
@@ -37,67 +87,144 @@ CUartCom::CUartCom(UART_HandleTypeDef *p_huart) : mp_huart(p_huart)
 }
 
 /**
- * @brief Construct UART communication object with half-duplex mode.
+ * @brief Enables UART reception with interrupts
+ * @note UART Interrupts must be enabled in the hardware configuration in order
+ * to use this method
  *
  * @param p_huart Pointer to UART hardware control register structure.
  * @param uart_de_port Pointer to GPIO port.
  * @param uart_de_pin GPIO pin.
  */
-CUartCom::CUartCom(UART_HandleTypeDef *p_huart,
-                   GPIO_TypeDef *uart_de_port,
-                   uint16_t uart_de_pin)
-    : mp_huart(p_huart),
-      m_uart_de_port(uart_de_port),
-      m_uart_de_pin(uart_de_pin)
+void CUartCom::startRx()
 {
-    // Check mp_huart is not null
-    if (!mp_huart)
+    HAL_UART_Receive_IT(mp_huart, &m_rx_char, 1);
+}
+
+/**
+ * @brief Start UART Transmission and push message to the TX Queue
+ * @param msg Message to send.
+ * TODO: Add return value (True if there was space in the queue)
+ */
+void CUartCom::send(const etl::string<MAX_STRING_SIZE> msg)
+{
+    bool event = NO_MESSAGE_AVAILABLE;
+
+    if (m_tx_queue.size() <= MAX_TX_QUEUE_SIZE && (msg.empty() == false))
     {
-        // Hang application with a toggling ALARM Pin
-        while (1)
+        m_tx_queue.put(msg);
+    }
+
+    if (m_tx_queue.size() > 0)
+    {
+        event = MESSAGE_AVAILABLE;
+    }
+
+    if (m_status == IDLE)
+    {
+        if (event == NO_MESSAGE_AVAILABLE)
         {
-            // TODO: cannot hardcode stuff like this into classes.
-            //            HAL_GPIO_TogglePin(ALARM_GPIO_Port, ALARM_Pin);
-            HAL_Delay(200);
+            endTx();
+        }
+        else if (event == MESSAGE_AVAILABLE)
+        {
+            updateTxBuffer();
+            transmit();
         }
     }
 }
 
 /**
- * @brief sends message via UART.
- * @param msg Message to send.
+ * @brief Update TX Buffer with next element on queue
  */
-void CUartCom::send(const etl::string<MAX_STRING_SIZE> &msg)
+void CUartCom::updateTxBuffer()
 {
-    uint16_t msg_len = msg.length();
+    etl::string<MAX_STRING_SIZE> message = m_tx_queue.get();
+    m_tx_msg_length = message.length();
+    strcpy(m_tx_buffer, message.c_str());
+    m_status = TX;
+}
 
-    // Enable UART_DE Pin
-    if (m_uart_de_port != nullptr)
+void CUartCom::transmit()
+{
+    if (mb_half_duplex)
     {
-        HAL_GPIO_WritePin(m_uart_de_port, m_uart_de_pin, GPIO_PIN_SET);
+        // Disable RX Interrupts
+        // Disable the UART Parity Error Interrupt
+        __HAL_UART_DISABLE_IT(mp_huart, UART_IT_PE);
+
+        // Disable the UART Error Interrupt: (Frame error, noise error, overrun
+        // error)
+        __HAL_UART_DISABLE_IT(mp_huart, UART_IT_ERR);
+
+        // Disable the UART Data Register not empty Interrupt
+        __HAL_UART_DISABLE_IT(mp_huart, UART_IT_RXNE);
+    }
+    // Enable USART_DE pin
+    m_uart_de_pin.set(true);
+    HAL_UART_Transmit_IT(mp_huart, (uint8_t *)m_tx_buffer, m_tx_msg_length);
+}
+
+/**
+ * @brief Ends UART TX and disables UART DE Pin
+ */
+void CUartCom::endTx()
+{
+    if (mb_half_duplex)
+    {
+        // Re-enable RX Interrupts
+        // Enable the UART Parity Error Interrupt
+        __HAL_UART_ENABLE_IT(mp_huart, UART_IT_PE);
+
+        // Enable the UART Error Interrupt: (Frame error, noise error, overrun
+        // error)
+        __HAL_UART_ENABLE_IT(mp_huart, UART_IT_ERR);
+
+        // Enable the UART Data Register not empty Interrupt
+        __HAL_UART_ENABLE_IT(mp_huart, UART_IT_RXNE);
     }
 
-    // Convert string to C style
-    const char *c_msg = msg.c_str();
+    // Disable TX interrupt, disable pin
+    __HAL_UART_DISABLE_IT(mp_huart, UART_IT_TXE);
+    m_uart_de_pin.set(false);
+    m_status = IDLE;
+}
 
-    // Check it is a valid string and send error message if not
-    if (!(*c_msg) || c_msg == nullptr)
+/**
+ * @brief UART Reception Complete Interrupt Handler
+ * @arg p_huart Pointer to UART Handler
+ */
+void CUartCom::uartRxHandler(UART_HandleTypeDef *p_huart)
+{
+    // Store incoming char
+    static uint8_t len_counter = 0;
+    /**
+     * @note The full_buffer flag is TRUE when the counter
+     * reaches (RX_BUF_SIZE -1) because 2 extra characters are
+     * needed for parsing. (See getString())
+     */
+    bool full_buffer = (len_counter >= (RX_BUF_SIZE));
+    if (m_rx_char == '\n' || m_rx_char == '\r')
     {
-        const uint8_t error_msg_len = 28;
-        const char *error_msg = "Invalid string from c_str()\n";
-        HAL_UART_Transmit(mp_huart,
-                          (uint8_t *)error_msg,
-                          error_msg_len,
-                          UART_TIMEOUT);
+        // '\n' and '\r' are replaced with '\0' to mark the end of the string
+        m_rx_buffer.put('\0');
 
-        // Disable UART_DE Pin
-        if (m_uart_de_port != nullptr)
+        if (full_buffer)
         {
-            HAL_GPIO_WritePin(m_uart_de_port, m_uart_de_pin, GPIO_PIN_RESET);
+            /**
+             * @note If a command length exceeds the RX_BUF_SIZE, The whole
+             * command will be ignored.
+             */
+            // Check actual buffer length
+
+            // Send warning
+            send("[ERROR]: Command exceeds max. length\n");
+
+            // Reset buffer
+            m_rx_buffer.reset();
         }
         else
         {
-            std::string rx_string = getString();
+            etl::string<MAX_STRING_SIZE> rx_string = getString();
             if (m_rx_queue.size() <= MAX_RX_QUEUE_SIZE && !rx_string.empty())
             {
                 m_rx_queue.put(rx_string);
@@ -111,7 +238,19 @@ void CUartCom::send(const etl::string<MAX_STRING_SIZE> &msg)
         len_counter++;
     }
 
-        return;
+    HAL_UART_Receive_IT(p_huart, &m_rx_char, 1);
+}
+
+/**
+ * @brief UART Transmission Complete Interrupt Handler
+ * @arg p_huart Pointer to UART Handler
+ */
+void CUartCom::uartTxHandler(UART_HandleTypeDef *p_huart)
+{
+    bool event = NO_MESSAGE_AVAILABLE;
+    if (m_tx_queue.size() > 0)
+    {
+        event = MESSAGE_AVAILABLE;
     }
     // Check if queue is empty
     if (event == MESSAGE_AVAILABLE)
@@ -125,12 +264,102 @@ void CUartCom::send(const etl::string<MAX_STRING_SIZE> &msg)
     }
 }
 
-    // Send message
-    HAL_UART_Transmit(mp_huart, (uint8_t *)c_msg, msg_len, UART_TIMEOUT);
-
-    // Disable UART_DE Pin
-    if (m_uart_de_port != nullptr)
+/**
+ * @brief gets string stored in FIFO Buffer
+ * @return string
+ */
+etl::string<MAX_STRING_SIZE> CUartCom::getString()
+{
+    uint8_t counter = 0;
+    char c_string[RX_BUF_SIZE];
+    char data;
+    /**
+     * @note The counter needs to leave 2 free characters for the
+     *'\n' and '\0' characters, which are needed for the Tokenizer
+     */
+    while ((data = m_rx_buffer.get()) != '\0' && counter < (RX_BUF_SIZE - 1))
     {
-        HAL_GPIO_WritePin(m_uart_de_port, m_uart_de_pin, GPIO_PIN_RESET);
+        c_string[counter] = data;
+        counter++;
+    }
+    c_string[counter] = '\0';
+    etl::string<MAX_STRING_SIZE> cpp_string = (char *)c_string;
+
+    return cpp_string;
+}
+
+/**
+ * @brief Check if the queue has any commands to read
+ * @return True if the queue is not empty
+ */
+bool CUartCom::isCommandAvailable()
+{
+    if (m_rx_queue.size() > 0)
+    {
+        return true;
+    }
+    return false;
+}
+
+/**
+ * @brief Get and delete the first element in the RX queue
+ * @return Command as string
+ */
+etl::string<MAX_STRING_SIZE> CUartCom::getCommand()
+{
+    if (m_rx_queue.size() > 0)
+    {
+        etl::string<MAX_STRING_SIZE> command = m_rx_queue.get();
+        return command;
+    }
+    return "";
+}
+
+/**
+ * @brief get index for the active UART handler
+ * @return index from 0 to MAX_UART_ENGINES
+ */
+int8_t CUartCom::getIndex(UART_HandleTypeDef *p_huart)
+{
+    int8_t index = -1;
+    for (uint8_t i = 0; i < CUartCom::s_uart_instances; i++)
+    {
+        if (p_huart->Instance == CUartCom::sp_UART[i]->mp_huart->Instance)
+        {
+            index = i;
+        }
+    }
+    return index;
+}
+
+/**
+ * @brief UART Rx complete callback
+ */
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *p_huart)
+{
+    int8_t index = CUartCom::getIndex(p_huart);
+    if (index >= 0)
+    {
+        CUartCom::sp_UART[index]->uartRxHandler(p_huart);
+    }
+    else
+    {
+        Error_Handler();
+    }
+}
+
+/**
+ * @brief UART Tx complete callback
+ */
+void HAL_UART_TxCpltCallback(UART_HandleTypeDef *p_huart)
+{
+    int8_t index = CUartCom::getIndex(p_huart);
+    if (index >= 0)
+    {
+        CUartCom::sp_UART[index]->uartTxHandler(p_huart);
+    }
+    else
+    {
+        Error_Handler();
     }
 }
