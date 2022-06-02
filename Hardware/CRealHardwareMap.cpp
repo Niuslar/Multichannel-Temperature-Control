cefeb606ea1feb5a43c08091da7178158f1ffc50
Niuslar <niuslar@outlook.com>
2022-06-02 23:12:36 +0100

    feature: create skeleton classes that declare and implement abstraction of hardware access

---
+++ Hardware/CRealHardwareMap.cpp
@@ -0,0 +1,23 @@
+/**
+ * @file CRealHardwareMap.cpp
+ *
+ */
+
+/*
+ * CRealHardwareMap.cpp
+ *
+ *  Created on: 30 Apr 2022
+ *      Author: salavat.magazov
+ */
+
+#include "CRealHardwareMap.h"
+
+CRealHardwareMap::CRealHardwareMap()
+{
+    // TODO Auto-generated constructor stub
+}
+
+void CRealHardwareMap::init()
+{
+    /* all hardware initialisation of peripherals goes here. */
+}
