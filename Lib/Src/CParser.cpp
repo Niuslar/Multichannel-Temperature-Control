45e694638bc207bfe82cc8450f3a6d1bf5939074
Niuslar <niuslar@outlook.com>
2022-06-02 23:15:59 +0100

    wip

--- Lib/Src/CParser.cpp
+++ Lib/Src/CParser.cpp
@@ -37,80 +37,6 @@ CParser::parser_state_t CParser::parse(
 
         switch (current_token.type)
         {
-            case BEGIN_OPERATOR:
-                if (m_status == IDLE)
-                {
-                    m_status = BEGIN_KEY;
-                }
-                else
-                {
-                    m_status = ERROR_NESTED_JSON;
-                }
-                break;
-            case STRING_LITERAL:
-                if (m_status == BEGIN_KEY)
-                {
-                    // Store key
-                    m_command_name = m_tokens[i].text;
-                    m_status = END_KEY;
-                }
-                break;
-            case START_ARRAY_OP:
-                if (m_status == BEGIN_VALUE)
-                {
-                    m_status = BEGIN_ARRAY;
-                }
-                break;
-            case ARGUMENT_DELIMITER:
-                if (m_status == BEGIN_ARRAY || m_status == VALUE_ADDED)
-                {
-                    m_status = ADD_VALUE;
-                }
-                break;
-            case KEY_VALUE_DIV:
-                if (m_status == END_KEY)
-                {
-                    m_status = BEGIN_VALUE;
-                }
-                break;
-            case INTEGER:
-            case FLOAT:
-                if (m_status == BEGIN_VALUE)
-                {
-                    float number = std::stof(m_tokens[i].text.c_str());
-                    m_arguments[m_argument_counter] = number;
-                    m_argument_counter++;
-                    m_status = END_VALUES;
-                }
-                else if (m_status == BEGIN_ARRAY || m_status == ADD_VALUE)
-                {
-                    if (m_argument_counter < MAX_ARGUMENT_COUNT)
-                    {
-                        float number = std::stof(m_tokens[i].text.c_str());
-                        m_arguments[m_argument_counter] = number;
-                        m_argument_counter++;
-                        m_status = VALUE_ADDED;
-                    }
-                    else
-                    {
-                        m_status = ERROR_MAX_ARGS;
-                    }
-                }
-                break;
-            case END_ARRAY_OP:
-                if (m_status == VALUE_ADDED)
-                {
-                    m_status = END_VALUES;
-                }
-                break;
-            case END_OPERATOR:
-                if (m_status == END_VALUES)
-                {
-                    m_status = COMMAND_OK;
-                }
-                break;
-            default:
-                m_status = ERROR;
         }
     }
 
@@ -127,98 +53,93 @@ void CParser::getTokens(const etl::string<MAX_STRING_SIZE> &string)
     m_tokens.clear();
     m_token_counter = 0;
     token_t current_token;
+    bool b_exit_loop_flag = false;
 
     // Loop through characters in string
     for (char current_char : string)
     {
-        switch (current_char)
+        switch (current_token.type)
         {
-            // Numbers
-            case '0':
-            case '1':
-            case '2':
-            case '3':
-            case '4':
-            case '5':
-            case '6':
-            case '7':
-            case '8':
-            case '9':
-                if (current_token.type == WHITESPACE)
+            case WHITESPACE:
+                if (isdigit(current_char))
                 {
+                    current_token.text.append(1, current_char);
                     current_token.type = INTEGER;
-                    current_token.text.append(1, current_char);
                 }
-                else
+                else if (isSpecialChar(current_char))
                 {
                     current_token.text.append(1, current_char);
+                    current_token.type = SPECIAL_CHAR;
                 }
-                break;
-
-            // Check if it is a float
-            case '.':
-                if (current_token.type == INTEGER)
+                else if (current_char == '"')
                 {
-                    current_token.type = FLOAT;
-                    current_token.text.append(1, current_char);
-                }
-                break;
-
-            // Token end characters
-            case ' ':
-            case '\t':
-                if (current_token.type == STRING_LITERAL)
-                {
-                    current_token.text.append(1, current_char);
-                }
-                else
-                {
-                    endToken(current_token);
-                }
-                break;
-            case '\n':
-            case '\r':
-            case '\0':
-                endToken(current_token);
-                break;
-
-            // Operators
-            case ':':
-                addOperator(current_token, KEY_VALUE_DIV, current_char);
-                break;
-            case '{':
-                addOperator(current_token, BEGIN_OPERATOR, current_char);
-                break;
-            case '}':
-                addOperator(current_token, END_OPERATOR, current_char);
-                break;
-            case '[':
-                addOperator(current_token, START_ARRAY_OP, current_char);
-                break;
-            case ']':
-                addOperator(current_token, END_ARRAY_OP, current_char);
-                break;
-            case ',':
-                addOperator(current_token, ARGUMENT_DELIMITER, current_char);
-                break;
-
-            // String literal
-            case '"':
-                if (current_token.type != STRING_LITERAL)
-                {
-                    endToken(current_token);
                     current_token.type = STRING_LITERAL;
                 }
                 else
                 {
+                    // We append a character because we need to store the
+                    // invalid token for the parser
+                    current_token.text.append(1, current_char);
+                    current_token.type = INVALID;
+                }
+
+            case INTEGER:
+                if (isdigit(current_char))
+                {
+                    current_token.text.append(1, current_char);
+                }
+                else if (isSpecialChar(current_char))
+                {
+                    endToken(current_token);
+                    current_token.text.append(1, current_char);
+                    current_token.type = SPECIAL_CHAR;
+                }
+                else if (current_char == '.')
+                {
+                    current_token.text.append(1, current_char);
+                }
+                else if (current_char == '"')
+                {
+                    endToken(current_token);
+                    current_token.type = STRING_LITERAL;
+                }
+                else if (current_char == '\n' || current_char == '\0' ||
+                         current_char == ' ')
+                {
                     endToken(current_token);
                 }
-                break;
 
-            default:
-                if (current_token.type == WHITESPACE ||
-                    current_token.type == INTEGER ||
-                    current_token.type == FLOAT)
+            case FLOAT:
+                if (isdigit(current_char))
+                {
+                    current_token.text.append(1, current_char);
+                }
+                else if (isSpecialChar(current_char))
+                {
+                    endToken(current_token);
+                    current_token.text.append(1, current_char);
+                    current_token.type = SPECIAL_CHAR;
+                }
+                else if (current_char == '"')
+                {
+                    endToken(current_token);
+                    current_token.type = STRING_LITERAL;
+                }
+                else if (current_char == '\n' || current_char == '\0' ||
+                         current_char == ' ')
+                {
+                    endToken(current_token);
+                }
+                else
+                {
+                    endToken(current_token);
+                    current_token.text.append(1, current_char);
+                    current_token.type = INVALID;
+                }
+
+            case STRING:
+                if (current_char == '\n' || current_char == '\0' ||
+                    current_char == '"' || current_char == ' ')
                 {
                     endToken(current_token);
                 }
@@ -226,7 +147,46 @@ void CParser::getTokens(const etl::string<MAX_STRING_SIZE> &string)
                 {
                     current_token.text.append(1, current_char);
                 }
-                break;
+            case SPECIAL_CHAR:
+                if (isdigit(current_char))
+                {
+                    endToken(current_token);
+                    current_token.text.append(1, current_char);
+                    current_token.type = INTEGER;
+                }
+                if (isSpecialChar(current_char))
+                {
+                    endToken(current_token);
+                    current_token.text.append(1, current_char);
+                }
+                else if (current_char == '"')
+                {
+                    endToken(current_token);
+                    current_token.type = STRING_LITERAL;
+                }
+                else if (current_char == '\n' || current_char == '\0' ||
+                         current_char == ' ')
+                {
+                    endToken(current_token);
+                }
+                else
+                {
+                    endToken(current_token);
+                    current_token.text.append(1, current_char);
+                    current_token.type = INVALID;
+                }
+            case INVALID:
+                // Set flag to exit loop, there was an error
+                endToken(current_token);
+                b_exit_loop_flag = true;
+            default:
+                endToken(current_token);
+                current_token.text.append(1, current_char);
+                current_token.type = INVALID;
+        }
+        if (b_exit_loop_flag == true)
+        {
+            break;
         }
     }
 }
@@ -247,29 +207,6 @@ void CParser::endToken(CParser::token_t &token)
     }
     token.type = WHITESPACE;
     token.text.clear();
-}
-
-/**
- * @brief Add operator token
- * @param token
- * @param operator type
- * @param character to be appended
- */
-void CParser::addOperator(CParser::token_t &token,
-                          CParser::token_type_t operator_type,
-                          char character)
-{
-    if (token.type != STRING_LITERAL)
-    {
-        endToken(token);
-        token.type = operator_type;
-        token.text.append(1, character);
-        endToken(token);
-    }
-    else
-    {
-        token.text.append(1, character);
-    }
 }
 
 /**
@@ -306,4 +243,24 @@ float CParser::operator[](unsigned int index)
         return m_arguments[index];
     }
     return ERROR_VALUE;
+}
+
+/**
+ * @brief Checks if a character is a special character used in JSON
+ *
+ * @param character
+ *
+ * @return True if it is a special character
+ */
+bool CParser::isSpecialChar(char character)
+{
+    bool b_is_special = false;
+    for (char i : m_special_characters)
+    {
+        if (character == i)
+        {
+            b_is_special = true;
+        }
+    }
+    return b_is_special;
 }
