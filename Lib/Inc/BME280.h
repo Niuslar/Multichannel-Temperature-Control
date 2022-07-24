674806b5356862fe9bde107d027f5ed5eab0b8b5
Salavat Magazov <s.magazov@gmail.com>
2022-07-24 13:11:52 +0100

    feature: add module for BME280 sensor.

---
+++ Lib/Inc/BME280.h
@@ -0,0 +1,17 @@
+/**
+ *@file BME280.h
+ *@brief This file contains the API functions for accessing BME280 sensor.
+ *
+ */
+
+/*
+ * BME280.h
+ *
+ *  Created on: 24 Jul 2022
+ *      Author: salavat.magazov
+ */
+
+#ifndef INC_BME280_H_
+#define INC_BME280_H_
+
+#endif /* INC_BME280_H_ */
---
+++ Lib/Src/BME280.c
@@ -0,0 +1,12 @@
+/**
+ *@file BME280.c
+ *@brief This file contains the BME280 sensor driver code.
+ *
+ */
+
+/*
+ * BME280.c
+ *
+ *  Created on: 24 Jul 2022
+ *      Author: salavat.magazov
+ */
