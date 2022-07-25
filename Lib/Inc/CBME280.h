852b544cf8f14073be3bd022e41db6f81dc6bedd
Salavat Magazov <s.magazov@gmail.com>
2022-07-25 08:50:44 +0100

    feature: add sensor initialisation method

---
+++ Lib/Inc/CBME280.h
@@ -0,0 +1,84 @@
+/**
+ *@file CBME280.h
+ *
+ */
+
+/*
+ * CBME280.h
+ *
+ *  Created on: 24 Jul 2022
+ *      Author: salavat.magazov
+ */
+
+#ifndef CBME280_H_
+#define CBME280_H_
+
+#include "CGpioWrapper.h"
+#include "gpio.h"
+#include "spi.h"
+
+#define T_CALIBRATION_SIZE 3
+#define P_CALIBRATION_SIZE 9
+#define H_CALIBRATION_SIZE 6
+
+class CBME280
+{
+    enum REGISTER_MAP
+    {
+        /* Calibration registers. */
+        DIG_T1 = 0x88,
+        DIG_T2 = 0x8A,
+        DIG_T3 = 0x8C,
+        DIG_P1 = 0x8E,
+        DIG_P2 = 0x90,
+        DIG_P3 = 0x92,
+        DIG_P4 = 0x94,
+        DIG_P5 = 0x96,
+        DIG_P6 = 0x98,
+        DIG_P7 = 0x9A,
+        DIG_P8 = 0x9C,
+        DIG_P9 = 0x9E,
+        DIG_H1 = 0xA1,
+        DIG_H2 = 0xE1,
+        DIG_H3 = 0xE3,
+        DIG_H4 = 0xE4,
+        DIG_H5 = 0xE5,
+        /* Sensor data registers. */
+        ID = 0xD0,
+        RESET = 0xE0,
+        CTRL_HUM = 0xF2,
+        STATUS = 0xF3,
+        CTRL_MEAS = 0xF4,
+        CONFIG = 0xF5,
+        PRESS_MSB = 0xF7,
+        PRESS_LSB = 0xF8,
+        PRESS_XLSB = 0xF9,
+        TEMP_MSB = 0xFA,
+        TEMP_LSB = 0xFB,
+        TEMP_XLSB = 0xFC,
+        HUM_MSB = 0xFD,
+        HUM_LSB = 0xFE
+    };
+
+public:
+    CBME280(SPI_HandleTypeDef *p_spi,
+            GPIO_TypeDef *p_slave_select_port,
+            uint16_t slave_select_pin);
+    virtual ~CBME280();
+
+    bool init();
+
+private:
+    void calibrateSensor(uint8_t const *const p_calibration_data);
+
+    SPI_HandleTypeDef *mp_spi;
+    CGpioWrapper m_slave_select;
+
+    bool mb_initialised;
+
+    uint16_t m_temperature_calibration[T_CALIBRATION_SIZE];
+    uint16_t m_pressure_calibration[P_CALIBRATION_SIZE];
+    uint16_t m_humidity_calibration[H_CALIBRATION_SIZE];
+};
+
+#endif /* CBME280_H_ */
---
+++ Lib/Src/CBME280.cpp
@@ -0,0 +1,148 @@
+/**
+ *@file CBME280.cpp
+ *
+ */
+
+/*
+ * BME280.cpp
+ *
+ *  Created on: 24 Jul 2022
+ *      Author: salavat.magazov
+ */
+
+#include <CBME280.h>
+
+#define BME280_ID             0x60
+#define BMP280_ID             0x58
+#define SOFT_RESET            0xB6
+#define SPI_WRITE_MASK        0x80
+#define ID_PACKET_SIZE        2
+#define CALIB1_PACKET_SIZE    24
+#define CALIB2_PACKET_SIZE    1
+#define CALIB3_PACKET_SIZE    8
+#define SPI_TIMEOUT_MS        100
+#define SENSOR_RESET_DELAY_MS 9
+
+#define CONCAT_BYTES(msb, lsb) (((uint16_t)msb << 8) | (uint16_t)lsb)
+
+#define HIGH true
+#define LOW  false
+
+CBME280::CBME280(SPI_HandleTypeDef *p_spi,
+                 GPIO_TypeDef *p_slave_select_port,
+                 uint16_t slave_select_pin)
+    : mp_spi(p_spi),
+      m_slave_select(p_slave_select_port, slave_select_pin),
+      mb_initialised(false)
+{
+    if (mp_spi == nullptr)
+    {
+        Error_Handler();
+    }
+    m_slave_select.set(true);
+}
+
+CBME280::~CBME280()
+{
+    // TODO Auto-generated destructor stub
+}
+
+bool CBME280::init()
+{
+    /* TODO:
+     * 1. Check SPI works.
+     * 2. Read chip ID to verify comms to sensor works.
+     * 3. Read calibration values and store in appropriate places.
+     */
+    /* This communication via SPI is going to be done without interrupts.*/
+    // TODO: how do we ensure SPI is available? Perhaps need a wrapper around
+    // SPI with a mutex.
+    if (HAL_SPI_GetState(mp_spi) != HAL_SPI_STATE_READY)
+    {
+        Error_Handler();
+    }
+    uint8_t register_address;
+    /* Soft reset. */
+    register_address = RESET;
+    m_slave_select.set(false);
+    HAL_SPI_Transmit(mp_spi, &register_address, 1, SPI_TIMEOUT_MS);
+    m_slave_select.set(true);
+    HAL_Delay(SENSOR_RESET_DELAY_MS);
+    /* Read sensor ID to see if it is responding to comms. */
+    register_address = ID;
+    uint8_t id_register[ID_PACKET_SIZE];
+    m_slave_select.set(LOW);
+    HAL_SPI_TransmitReceive(mp_spi,
+                            &register_address,
+                            id_register,
+                            ID_PACKET_SIZE,
+                            SPI_TIMEOUT_MS);
+    m_slave_select.set(HIGH);
+    if ((id_register[1] != BME280_ID) && (id_register[1] != BMP280_ID))
+    {
+        return false;
+    }
+    /* Read sensor calibration data. */
+    register_address = DIG_T1;
+    uint8_t calibration_register[CALIB1_PACKET_SIZE +
+                                 CALIB2_PACKET_SIZE * CALIB3_PACKET_SIZE];
+    m_slave_select.set(LOW);
+    HAL_SPI_TransmitReceive(mp_spi,
+                            &register_address,
+                            calibration_register,
+                            CALIB1_PACKET_SIZE,
+                            SPI_TIMEOUT_MS);
+    m_slave_select.set(HIGH);
+    register_address = DIG_H1;
+    m_slave_select.set(LOW);
+    HAL_SPI_TransmitReceive(mp_spi,
+                            &register_address,
+                            &(calibration_register[CALIB1_PACKET_SIZE - 1]),
+                            CALIB2_PACKET_SIZE,
+                            SPI_TIMEOUT_MS);
+    m_slave_select.set(HIGH);
+    register_address = DIG_H2;
+    m_slave_select.set(LOW);
+    HAL_SPI_TransmitReceive(mp_spi,
+                            &register_address,
+                            &(calibration_register[CALIB1_PACKET_SIZE]),
+                            CALIB2_PACKET_SIZE,
+                            SPI_TIMEOUT_MS);
+    m_slave_select.set(HIGH);
+    calibrateSensor(calibration_register);
+    mb_initialised = true;
+    return true;
+}
+
+void CBME280::calibrateSensor(uint8_t const *const p_calibration_data)
+{
+    uint8_t const *p_runner = p_calibration_data;
+    for (int i = 0; i < T_CALIBRATION_SIZE; i++)
+    {
+        m_temperature_calibration[i] = *p_runner;
+        m_temperature_calibration[i] |= *(++p_runner) << 8;
+        ++p_runner;
+    }
+    for (int i = 0; i < P_CALIBRATION_SIZE; i++)
+    {
+        m_pressure_calibration[i] = *p_runner;
+        m_pressure_calibration[i] |= *(++p_runner) << 8;
+        ++p_runner;
+    }
+    /* Humidity calibration data is spread around and is stored in a weird
+     * convoluted way. See the manual. This recovery was copied with
+     * modifications from the Bosch github repository.
+     * https://github.com/BoschSensortec/BME280_driver */
+    /* I am certain the engineer at Bosch who came up with this had management
+     * breathing down their neck. One day they stopped caring and made this
+     * monstrocity.*/
+    m_humidity_calibration[0] = p_calibration_data[25];
+    m_humidity_calibration[1] =
+        (p_calibration_data[27] << 8) + p_calibration_data[26];
+    m_humidity_calibration[2] = p_calibration_data[28];
+    m_humidity_calibration[3] =
+        (p_calibration_data[29] << 4) + (p_calibration_data[30] & 0x0F);
+    m_humidity_calibration[4] =
+        (p_calibration_data[31] << 4) + (p_calibration_data[30] >> 4);
+    m_humidity_calibration[5] = p_calibration_data[32];
+}
