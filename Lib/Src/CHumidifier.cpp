8d908d42f52733424dd3931d27d4f7cf390e2f89
Salavat Magazov <s.magazov@gmail.com>
2022-09-26 16:27:46 +0100

    feature: add skeleton of the humidifier class.

---
+++ Lib/Src/CHumidifier.cpp
@@ -0,0 +1,36 @@
+/**
+ * @file CHumidifier.cpp
+ *
+ */
+
+/*
+ * CHumidifier.cpp
+ *
+ *  Created on: 26 Sep 2022
+ *      Author: salavat.magazov
+ */
+
+#include "CHumidifier.h"
+
+CHumidifier::CHumidifier()
+{
+    // TODO Auto-generated constructor stub
+}
+
+CHumidifier::~CHumidifier()
+{
+    // TODO Auto-generated destructor stub
+}
+
+/**
+ * @brief Add some quantity of vapour to the system.
+ *
+ * @param quantity Amount of vapour that is required.
+ *
+ * @note At this point it is unclear how the humidifier will work, so quantity
+ * is somewhat ill-defined.
+ */
+void CHumidifier::addVapour(float quantity)
+{
+    return;
+}
